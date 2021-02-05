b3be5c86c324417f719e481ec92106e88b2a7a7c
Dakshitha Suriyaaratchie <dakshitha.18@cse.mrt.ac.lk>
2021-02-06 02:05:13 +0530

    Redux

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,14 @@ const mysql = require("mysql");
 const dotenv = require("dotenv");
 dotenv.config();
 const mysqlConnection = mysql.createConnection({
-    // host: "localhost",
-    // user: "root",
-    // password: "dakshitha",
-    // database: "dbproject",
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_SCHEME,
+    host: "localhost",
+    user: "root",
+    password: "dakshitha",
+    database: "dbproject",
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_SCHEME,
     multipleStatements: true,
   });
   mysqlConnection.connect((err) => {
---
+++ frontend/src/store.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import {producListReducer} from './reducers/productreducers'
+
+const reducer = combineReducers({
+    productList:producListReducer
+});
+const initialState = {};
+const middleware = [thunk];
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+export default store;
