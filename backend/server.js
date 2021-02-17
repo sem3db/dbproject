33d758f49b1b1077d07a09b8b36324ff3f07e26c
DulajKavinda98 <kavindadulaj.1998@gmail.com>
2021-02-17 23:49:51 +0530

    category and variant gets

--- backend/server.js
+++ backend/server.js
@@ -15,17 +15,17 @@ app.use(express.urlencoded({ extended: true }));
 // /api/products -> PRODUCT OVERVIEW (MOST DEMANDED, LATEST PRODUCTS)
 // /api/products(category) -> PRODUCTS OF PARTICULAR category
 // /api/product/id -> PARTICULAR PRODUCT DETAILS
-const products = require("./data/products");
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/product/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+// const products = require("./data/products");
+// app.get("/", (req, res) => {
+//   res.send("API is running");
+// });
+// app.get("/api/products", (req, res) => {
+//   res.json(products);
+// });
+// app.get("/api/product/:id", (req, res) => {
+//   const product = products.find((p) => p._id === req.params.id);
+//   res.json(product);
+// });
 // =================================================================================================================
 app.use('/api/customer', userRouter);
 app.use('/api/products', productRouter);
