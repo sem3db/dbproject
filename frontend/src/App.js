f249c90bd94ebf37b2303e5ae99d40e3e6b89d97
danukadj <danukasandaruwandj@gmail.com>
2021-02-25 18:27:47 +0530

    merge errors correcting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,18 +17,6 @@ import ProductListScreen from './screens/ProductListScreen';
 import OrderListScreen from './screens/OrderListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
 import ProfileScreen from './screens/ProfileScreen';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
-import CartScreen from "./screens/CartScreen";
-import LoginScreen from "./screens/LoginScreen";
-import ShippingScreen from "./screens/ShippingScreen";
-import PaymentScreen from "./screens/PaymentScreen";
-import PlaceOrderScreen from "./screens/PlaceOrderScreen";
-import ProductListScreen from "./screens/ProductListScreen";
-import OrderListScreen from "./screens/OrderListScreen";
-import ProductEditScreen from "./screens/ProductEditScreen";
 
 const App = () => {
   return (
@@ -51,25 +39,6 @@ const App = () => {
         <Route path='/order/:id' component={OrderScreen}/>
         <Route path='/productlist' component={ProductListScreen}></Route>
         <Route path='/orderlist' component={OrderListScreen}></Route>
-      <Header />
-      <main className="py-3">
-        <Container>
-          {/* <HomeScreen/> */}
-          <Route path="/" component={HomeScreen} exact />
-          <Route path="/products/:cat" component={HomeScreen} exact />
-          <Route path="/product/:id" component={ProductScreen} exact />
-          <Route
-            path="/product/:id/edit"
-            component={ProductEditScreen}
-            exact
-          ></Route>
-          <Route path="/cart/:id?" component={CartScreen} />
-          <Route path="/login" component={LoginScreen} />
-          <Route path="/shipping" component={ShippingScreen} />
-          <Route path="/payment" component={PaymentScreen} />
-          <Route path="/placeorder" component={PlaceOrderScreen} />
-          <Route path="/productlist" component={ProductListScreen}></Route>
-          <Route path="/orderlist" component={OrderListScreen}></Route>
         </Container>
       </main>
       <Footer />
