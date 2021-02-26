b83d8d4ad5cfe7b607941b4816183badba19c095
DulajKavinda98 <kavindadulaj.1998@gmail.com>
2021-02-27 01:51:57 +0530

    procedure updates-findProductById

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -7,45 +7,49 @@ const ACCESS_TOKEN_SECRECT = "DBProject";
 
 async function findProductById(id) {
   try {
-    const productDetail = await customerExecuteSQL(
-      "SELECT product_id, product_name , category_id, subcat_id, description, weight, dimension, brand FROM product WHERE product_id =?",
-      [parseInt(id)]
-    );
+    const productFetched = await customerExecuteSQL('call getProductById(?)',[parseInt(id)]).then();
 
-    const a_variant = (
-      await customerExecuteSQL(
-        "SELECT variant_Id , SKU , image_url ,price, offer, color,size, no_stock FROM variant WHERE product_id =? LIMIT 1",
-        [parseInt(id)]
-      )
-    )[0];
+    var productDetail=JSON.parse(JSON.stringify(productFetched[0][0]));
+    var colors=JSON.parse(JSON.stringify(productFetched[1]));
+    var sizes=JSON.parse(JSON.stringify(productFetched[2]));
+    var a_variant=JSON.parse(JSON.stringify(productFetched[3][0]));
+    var category=JSON.parse(JSON.stringify(productFetched[4][0]));
+    var sub_category=JSON.parse(JSON.stringify(productFetched[5][0]));
+
+    var dis_colors = [];
+    for(i=0;i<colors.length;i++){
+      dis_colors.push(colors[i].color);
+    }
+
+    var dis_sizes=[];
+    for(i=0;i<sizes.length;i++){
+      dis_sizes.push(sizes[i].size);
+    }
 
     const product = {};
-    product.product_id = productDetail[0].product_id;
-    product.product_name = productDetail[0].product_name;
-    product.description = productDetail[0].description;
-    product.weight = productDetail[0].weight;
-    product.dimension = productDetail[0].dimension;
-    product.brand = productDetail[0].brand;
-    product.category = (
-      await customerExecuteSQL(
-        "SELECT category_name FROM category WHERE category_id=?",
-        [parseInt(productDetail[0].category_id)]
-      )
-    )[0].category_name;
-    product.subCategory = (
-      await customerExecuteSQL(
-        "SELECT subcat_name FROM subcategory WHERE subcat_id=?",
-        [parseInt(productDetail[0].subcat_id)]
-      )
-    )[0].subcat_name;
-    product.color = a_variant.color;
-    product.size = a_variant.size;
-    product.noStock = a_variant.no_stock;
-    product.imageUrl = a_variant.image_url;
-    product.price = a_variant.price;
+    product.product_id = id;
+    product.product_name = productDetail.product_name;
+    product.description = productDetail.description;
+    product.weight = productDetail.weight;
+    product.dimension = productDetail.dimension;
+    product.brand = productDetail.brand;
+    product.category = category.category_name;
+    product.subCategory = sub_category.subcat_name;
+    product.colors=dis_colors;
+    product.sizes=dis_sizes;
+    product.Onevariant={
+      color:a_variant.color,
+      size:a_variant.size,
+      noStock:a_variant.no_stock,
+      imageUrl:a_variant.image_url,
+      price:a_variant.price
+    };    
+
     return product;
+
   } catch (e) {
-    return "Product Not Found";
+      console.log("Error :", JSON.parse(JSON.stringify(e))["error"]);
+      return "Product Not Found";
   }
 }
 
