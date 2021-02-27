4bcfac2d7944c2926e654832b3f993390e85643f
DulajKavinda98 <kavindadulaj.1998@gmail.com>
2021-02-27 14:19:20 +0530

    database files changes

--- backend/database/database_files/Func&ProcCustomerPreviledges.sql
+++
@@ -1,9 +0,0 @@
-#previledges for customer for func&procedures
-
-GRANT EXECUTE ON PROCEDURE cse_21.registerCustomer TO 'customer'@'localhost';
-
-GRANT EXECUTE ON FUNCTION cse_21.userAlreadyRegistered TO 'customer'@'localhost';
-
-GRANT EXECUTE ON PROCEDURE cse_21.getProductById TO 'customer'@'localhost';
-
-FLUSH PRIVILEGES;
