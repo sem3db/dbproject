13fac817aedf3092249aa97caee1c6d75ac53b7e
DulajKavinda98 <kavindadulaj.1998@gmail.com>
2021-03-01 12:35:18 +0530

    address state and city template

--- backend/models/customerModel.js
+++ backend/models/customerModel.js
@@ -119,11 +119,40 @@ async function updateCustomer(
 
 async function getShippingAddress(id) {
   try {
+
+    const CustomerInfo = {};
+
+    const States = await customerExecuteSQL(
+      "SELECT DISTINCT state FROM registered_customer"
+    );
+
+    var disStateNCity = {};
+    for (var i = 0; i < States.length; i++) {
+      var Cities = await customerExecuteSQL(
+        "SELECT DISTINCT city FROM registered_customer WHERE state=?",
+        [States[i].state]
+      );
+
+      var discity=[];
+      for (var j = 0; j < Cities.length;j++) {
+        discity.push(Cities[j].city);
+      }
+      disStateNCity[States[i].state]=discity;
+    }
+
+
+    CustomerInfo.States = disStateNCity;
+
     const addressFetched = await customerExecuteSQL("SELECT zip_code, address_line_1, address_line_2, city, state, phone FROM registered_customer WHERE reg_customer_id=?", [
       parseInt(id),
     ]).then();
 
-    return addressFetched[0];
+    
+
+    CustomerInfo.Customer=addressFetched[0];
+
+    return CustomerInfo;
+    
   } catch (e) {
     console.log("Error :", JSON.parse(JSON.stringify(e))["error"]);
   }
@@ -160,5 +189,9 @@ async function updateShippingAddress(
   }
   
 }
+
+
+
+
 
 module.exports = { loginIn, register,findCustomerById ,updateCustomer,getShippingAddress,updateShippingAddress};
