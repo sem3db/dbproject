815706693cabb188b434b7f5d52cff7c61673012
Kavindu <kavinduravishka98sl@gmail.com>
2021-03-02 08:47:05 +0530

    fixed errors dbchanges.sql

--- backend/database/database_files/dbchanges.sql
+++ backend/database/database_files/dbchanges.sql
@@ -25,12 +25,13 @@ ALTER TABLE `cse_21`.`ProductOrder`
 ADD COLUMN `order_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP AFTER `additional_notes`;
 
 
--- ALTER TABLE `cse_21`.`productorder` 
--- CHANGE COLUMN `order_date` `order_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP AFTER `order_id`;
+ALTER TABLE `cse_21`.`ProductOrder` 
+CHANGE COLUMN `order_date` `order_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP AFTER `order_id`;
 
 
 
 --new table main_city
+
 CREATE TABLE `cse_21`.`main_city` (
   `city_id` INT NOT NULL AUTO_INCREMENT,
   `city_name` VARCHAR(200) NULL,
@@ -54,17 +55,17 @@ INSERT INTO `cse_21`.`main_city` (`city_name`) VALUES ('Mannar');
 
 
 --product order table
-ALTER TABLE `cse_21`.`productorder` 
+ALTER TABLE `cse_21`.`ProductOrder` 
 DROP COLUMN `delivery_estimate`;
 
-ALTER TABLE `cse_21`.`productorder` 
+ALTER TABLE `cse_21`.`ProductOrder` 
 ADD COLUMN `delivery_estimate` DATE NULL AFTER `additional_notes`;
 
-ALTER TABLE `cse_21`.`productorder` 
+ALTER TABLE `cse_21`.`ProductOrder` 
 CHANGE COLUMN `delivery_estimate` `delivery_estimate` DATE NULL DEFAULT NULL AFTER `delivery_method`;
 
-UPDATE `cse_21`.`productorder` SET `delivery_estimate` = '2021-03-11' WHERE (`order_id` = '1');
-UPDATE `cse_21`.`productorder` SET `delivery_estimate` = '2021-03-11' WHERE (`order_id` = '2');
-UPDATE `cse_21`.`productorder` SET `delivery_estimate` = '2021-03-11' WHERE (`order_id` = '3');
-UPDATE `cse_21`.`productorder` SET `delivery_estimate` = '2021-03-11' WHERE (`order_id` = '4');
+UPDATE `cse_21`.`ProductOrder` SET `delivery_estimate` = '2021-03-11' WHERE (`order_id` = '1');
+UPDATE `cse_21`.`ProductOrder` SET `delivery_estimate` = '2021-03-11' WHERE (`order_id` = '2');
+UPDATE `cse_21`.`ProductOrder` SET `delivery_estimate` = '2021-03-11' WHERE (`order_id` = '3');
+UPDATE `cse_21`.`ProductOrder` SET `delivery_estimate` = '2021-03-11' WHERE (`order_id` = '4');
 
