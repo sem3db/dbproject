f5665380f08e07c23c7fdfc6250577b4d9a7f023
DulajKavinda98 <kavindadulaj.1998@gmail.com>
2021-03-02 19:58:02 +0530

    Vatiant search by params

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -73,7 +73,16 @@ async function findVariantByParams(product_id, color, size) {
       )
     )[0];
 
-    return variant;
+    const Variant = {
+      variantId: variant.variant_id,
+      color: variant.color,
+      size: variant.size,
+      noStock: variant.no_stock,
+      imageUrl: variant.image_url,
+      price: variant.price,
+    };
+
+    return Variant;
   } catch (e) {
     console.log("Error :", JSON.parse(JSON.stringify(e))["error"]);
   }
