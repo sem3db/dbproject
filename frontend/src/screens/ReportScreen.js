a396a869027edf89adb9c56d081de8dbf3f10a44
danukadj <danukasandaruwandj@gmail.com>
2021-03-03 21:31:30 +0530

    reports

--- frontend/src/screens/ReportScreen.js
+++ frontend/src/screens/ReportScreen.js
@@ -2,45 +2,54 @@ import React from 'react'
 
 export default function ReportScreen(props) {
     return (
-        <div className="admin">
+        <div className="">
             <h1>Reports</h1>
-            <div>
+            <div className="report Button">
                 <button
                 type="button"
-                className="small"
+                className=""
                 onClick={() =>
                     props.history.push(`/reports/report1`)
                 }
                 >
-                Report 1
-                </button>
+                Report 1 - Quarterly sales report
+                </button><br/><br/>
                 <button
                 type="button"
-                className="small"
+                className=""
                 onClick={() =>
                     props.history.push(`/reports/report2`)
                 }
                 >
-                Report 2
-                </button>
+                Report 2 - Products with most number of sales
+                </button><br/><br/>
                 <button
                 type="button"
-                className="small"
+                className=""
                 onClick={() =>
                     props.history.push(`/reports/report3`)
                 }
                 >
-                Report 3
-                </button>
+                Report 3 - Product category with most orders
+                </button><br/><br/>
                 <button
                 type="button"
-                className="small"
+                className=""
+                onClick={() =>
+                    props.history.push(`/productlist`)
+                }
+                >
+                Report 4 - most interest time period to a product
+                </button><br/><br/>
+                <button
+                type="button"
+                className=""
                 onClick={() =>
                     props.history.push(`/reports/report5`)
                 }
                 >
-                Report 5
-                </button>
+                Report 5 - Order report
+                </button><br/><br/>
             </div>
         </div>
     )
