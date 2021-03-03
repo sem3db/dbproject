941c9f4147b70d50f17ec3fa546428724ad05fdd
Kavindu <kavinduravishka98sl@gmail.com>
2021-03-03 11:12:32 +0530

    modified order model

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -66,7 +66,7 @@ async function moveToOrder_guest(
   try {
     const moveState = await customerExecuteSQL(
       "call moveToOrder_gst(?,?,?,?,?,?)",
-      [cust_id, paymethod, delstat, delmethod, note, productlist]
+      [cust_id, paymethod, delstat, delmethod, note, JSON.stringify(productlist)]
     ).then();
     console.log(moveState);
     return moveState;
