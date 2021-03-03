376b86168c6a46e1b316fcaebfc1e2a9261a1195
yazi99 <56828636+yazi99@users.noreply.github.com>
2021-03-03 22:12:46 +0530

    changes

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -16,17 +16,21 @@ async function getOrders() {
       var user;
       if (order.customer_type == "Registered") {
         user = await adminExecuteSQL(
-          "SELECT first_name from registered_customer where reg_customer_id=?",
+          "SELECT first_name, address_line_1, address_line_2, city, phone from customerdetails where reg_customer_id=?",
           [parseInt(order.customer_id)]
         );
       } else {
         user = await adminExecuteSQL(
-          "SELECT first_name from guest_customer where guest_id=?",
+          "SELECT first_name, address_line_1, address_line_2, city, phone from guest_customer where guest_id=?",
           [parseInt(order.customer_id)]
         );
       }
 
       order.user = user[0].first_name;
+      order.address_line_1 = user[0].address_line_1;
+      order.address_line_2 = user[0].address_line_2;
+      order.city = user[0].city;
+      order.phone = user[0].phone;
     }
     return orderData;
   } catch (e) {
@@ -62,7 +66,7 @@ async function moveToOrder_registered(
     console.log(moveState);
     return moveState;
   } catch (e) {
-    console.log(e)
+    console.log(e);
     console.log(JSON.parse(JSON.stringify(e))["error"]);
     return "Error";
   }
@@ -82,10 +86,23 @@ async function moveToOrder_guest(
   address_line_1,
   address_line_2,
   city,
-  state 
+  state
 ) {
   try {
-    const raw_cust_id = await customerExecuteSQL("call newGuest(?,?,?,?,?,?,?,?,?)",[email,phone,first_name,last_name,zip_code,address_line_1,address_line_2,city,state]).then();
+    const raw_cust_id = await customerExecuteSQL(
+      "call newGuest(?,?,?,?,?,?,?,?,?)",
+      [
+        email,
+        phone,
+        first_name,
+        last_name,
+        zip_code,
+        address_line_1,
+        address_line_2,
+        city,
+        state,
+      ]
+    ).then();
     const cust_id = JSON.parse(JSON.stringify(raw_cust_id))["cust_id"];
     const moveState = await customerExecuteSQL(
       "call moveToOrder_gst(?,?,?,?,?,?)",
