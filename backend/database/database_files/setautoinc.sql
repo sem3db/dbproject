dc2f75904630d228bf7b813f72be020437d34909
Kavindu <kavinduravishka98sl@gmail.com>
2021-03-03 14:18:06 +0530

    modified setautoinc.sql

--- backend/database/database_files/setautoinc.sql
+++ backend/database/database_files/setautoinc.sql
@@ -1,4 +1,5 @@
 use cse_21;
+alter TABLE cart_product modify column ID int NOT NULL AUTO_INCREMENT;
 SET @m = (SELECT MAX(ID) + 1 FROM cart_product); 
 SET @s = CONCAT('ALTER TABLE cart_product AUTO_INCREMENT=', @m);
 PREPARE stmt1 FROM @s;
